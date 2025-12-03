860a79c8e223b54e96ba84d75375e634a44b3260
citauka <daryaatsitova@icloud.com>
2025-12-03 21:47:47 +0100

    structure

---
+++ src/components/App/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className={css.app}>
+	<header className={css.toolbar}>
+		{/* Компонент SearchBox */}
+		{/* Пагінація */}
+		{/* Кнопка створення нотатки */}
+  </header>
+</div>
+
+    </>
+  )
+}
+
+export default App
